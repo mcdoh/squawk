65454009080ec90469a451ac5714344f4e05b784
Gavin McDonald <gavinmcdoh@gmail.com>
2018-02-04 03:49:29 +0000

    increased cookie max-length, should store session data in DB instead

--- config/config.exs
+++ config/config.exs
@@ -11,6 +11,7 @@ config :squawk,
 
 # Configures the endpoint
 config :squawk, SquawkWeb.Endpoint,
+  http: [port: 4000, protocol_options: [max_header_value_length: 65536]],
   url: [host: "localhost"],
   secret_key_base: "jftoctplvaS9tP2c4AS2weMpOMWiMejZnbqlCglD7+XzqHS0gJBU5aUpgHiQ1NSL",
   render_errors: [view: SquawkWeb.ErrorView, accepts: ~w(html json)],
