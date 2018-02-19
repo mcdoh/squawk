ad5a303350b115907fd8f1f65ceee22a08dc4058
Gavin McDonald <gavinmcdoh@gmail.com>
2018-02-19 19:59:11 +0000

    if ID from user doesn't exist in database (e.g. database rebuilt) give them a new ID

--- lib/squawk_web/plugs/load_user.ex
+++ lib/squawk_web/plugs/load_user.ex
@@ -1,13 +1,15 @@
 defmodule SquawkWeb.Plugs.LoadUser do
   import Plug.Conn
-  alias Squawk.Bird
   alias Squawk.Nest
+  alias Squawk.Bird
+  alias Squawk.Bird.User
 
   def init(_opts), do: nil
 
   def call(conn, _) do
     with user_id <- get_session(conn, :user_id),
-      true <- is_integer(user_id)
+      true <- is_integer(user_id),
+      %User{} = user <- Bird.get_user(user_id)
     do
       sqwks = Nest.get_user_squawks(user_id)
       conn |> assign(:sqwks, sqwks)
