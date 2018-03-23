8f74d111e0b8811ff8d565a1518901b10a0ca321
Gavin McDonald <gavinmcdoh@gmail.com>
2018-03-23 01:14:51 +0000

    naive CSS concatenation until webpack gets its act together

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,4 +1,21 @@
+const fs = require('fs');
 const path = require('path');
+
+let concatCSS = [{
+	target: '../priv/static/css/app.css',
+	files: [
+		path.join(__dirname, 'css', 'phoenix.css'),
+		path.join(__dirname, 'css', 'app.css'),
+	]
+}];
+
+concatCSS.forEach(css=> {
+	css.files.forEach((file, index) => {
+		let contents = fs.readFileSync(file, 'utf-8');
+
+		fs.writeFileSync(css.target, contents,  {flag: index === 0 ? 'w' : 'a'});
+	});
+});
 
 module.exports = {
 	entry: {
